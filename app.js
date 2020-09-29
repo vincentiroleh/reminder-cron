f40b82081d49d8072793ef53b682ce005915770f
Vincent Iroleh <irolehiroleh@gmail.com>
2020-09-29 14:28:18 +0100

    did stuff

---
+++ app.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const cron = require('node-cron');
+const mailer = require('./helper/node-mailer');
+
+app.get('/', (req, res) => {
+    res.send('Running a task every minute');
+});
+
+// running a task every minute
+try {
+    cron.schedule('* * * * *', () => {
+        console.log('Running task now');
+    
+        const email = 'blexnmirian@gmail.com';
+        const subject = 'Learning Reminder';
+        const msg = 'Time to code!'
+        mailer(email, subject, msg);
+    });
+} catch (error) {
+    console.log(error);
+}
+
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () =>
+    console.log(`running a task every minute on port ${PORT}`));
